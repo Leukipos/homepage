4b7bf87d27a10df752cd2e46e88a1eb11a203e46
Alex Zippel <alex.zippel@web.de>
2023-05-25 11:29:22 +0200

    ititial site

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
-  );
-}
+import styled from "styled-components";
+import Link from "next/link"
+
+const StyledLink = styled(Link)`
+  color: #334;
+  font-size: 2rem;
+  text-decoration: none;
+`
+export default function LandingPage() {
+  return(
+    <main> 
+      <StyledLink href="main">Alex Zippel</StyledLink>
+    </main>
+)}
+
+
+  
